e00b9ca4e02d2ac0315d5fd6e906d18dab0c5e68
PhongNguyen <phongnv0910@gmail.com>
2024-05-25 22:56:35 +0700

    add appsettings table

---
+++ NoteApp/App/Database/Data/AppSetting.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NoteApp.App.Database.Data
+{
+    public partial class AppSetting
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+       
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+    }
+}
